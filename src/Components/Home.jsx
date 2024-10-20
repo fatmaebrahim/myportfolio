4e1c8756e11d7e73cdf5b002b120df85dcd39fb9
fatmaebrahim <fatmaebrahim3131@gmail.com>
2024-10-20 20:16:16 +0300

    portfolio updated 2024

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
       <h1 className='text-[#FF6969] py-1 text-7xl font-semibold'>Fatma Ebrahim</h1>
       <h2 className='text-[#FFF5E0] py-3 text-3xl'>
         <span className='text-[#BB2525] py-3 text-3xl'>I'm a </span>
-        Frontend Developer and Computer Engineer</h2>
+        Computer Engineer & Software Developer</h2>
       
       <div onClick={handleClick} className='text-[#BB2525] py-3 my-3 px-3 w-[300px] border-2 border-[#BB2525] rounded-lg text-2xl font-semibold flex flex-row hover:cursor-pointer hover:border-[#FF6969] hover:text-[#FF6969] duration-500 '>
       <h1>Take a tour down here</h1>
